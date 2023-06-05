e9a967e6135870bd3f30321177bf2096382a10f1
Hunter7210 <114042717+Hunter7210@users.noreply.github.com>
2023-06-05 19:06:54 +0000

    05/06/2023

--- ExercicioPOO3/Elevador.java
+++ ExercicioPOO3/Elevador.java
@@ -1,43 +1,71 @@
 package ExercicioPOO3;
 
 public class Elevador {
-    //construir o elevador
-    //atributos do elevador
-    
+    // construir o elevador
+    // atributos do elevador
+
     int nMaxAndares;
     int nMaxPessoas;
     int andarAtual;
     int capacidadeAtual;
-    //construtor
+
+    // construtor
     public Elevador(int nMaxAndares, int nMaxPessoas) {
-        this.nMaxAndares = nMaxAndares;
+        this.nMaxAndares = nMaxAndares; // numero fixo
         this.nMaxPessoas = nMaxPessoas;
     }
-    
-    public int getnMaxAndares() {
-        return nMaxAndares;
-    }
-    public void setnMaxAndares(int nMaxAndares) {
-        this.nMaxAndares = nMaxAndares;
-    }
-    public int getnMaxPessoas() {
-        return nMaxPessoas;
-    }
-    public void setnMaxPessoas(int nMaxPessoas) {
-        this.nMaxPessoas = nMaxPessoas;
-    }
+
+    // contruir os metodos
+    // set and gets
     public int getAndarAtual() {
-        return andarAtual;
+        return andarAtual; // numero variavel
     }
+
     public void setAndarAtual(int andarAtual) {
         this.andarAtual = andarAtual;
     }
+
     public int getCapacidadeAtual() {
         return capacidadeAtual;
     }
+
     public void setCapacidadeAtual(int capacidadeAtual) {
         this.capacidadeAtual = capacidadeAtual;
     }
-    
 
+    // metodos adicionais
+    // iniciar
+    public void name() {
+        capacidadeAtual = 0;
+        andarAtual = 0;
+    }
+//entrar
+    public void entrar(int pessoas) {
+        if ((capacidadeAtual + pessoas) >= nMaxPessoas) {
+            System.out.println("Capacidade max atingida");
+            System.out.println("Pessoas para fora" + (capacidadeAtual + pessoas - nMaxPessoas));
+        }
+        else{
+            capacidadeAtual+=pessoas;
+        }
+    }
+//sair
+public void descer (int pessoas){
+    if (pessoas<capacidadeAtual) {
+        capacidadeAtual-=pessoas;
+    }
+    else{
+        System.out.println("Informe um numero valido, Quantidade atual: "+capacidadeAtual );
+        
+    }
+
+
+
+    /*DAVA PARA SER FEITO COM WHILE
+     while (pessoas>capacidadeAtual) {
+        
+    } */
+        
+    }
 }
+
